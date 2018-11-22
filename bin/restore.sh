16869829b3e50373b60882bf1686eca9e5a478e2
Andrei Ehrlich <andrei.ehrlich@corpuls.com>
2018-11-22 11:21:22 +0100

    simplify restore script
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- bin/restore.sh
+++ bin/restore.sh
@@ -23,24 +23,17 @@ DESCRIPTION:
 EOF
 }
 
+###############################################################################
+
 declare -A storage=""
 storage["eBooks"]="an3@nasbox.fritz.box:/home/an3/backup/docs/eBooks"
 
 ###############################################################################
 
 doRestoreBooks() {
-
-    for h in \
-        "nasbox" \
-        "192.168.178.25" \
-        "nasbox.fritz.box" \
-    ; do
-        printf "Trying to reach host [%s] ...\n" "${h}"
-        ping -c4 ${h} && break;
-    done \
-    && doRsync \
+    doRsync \
         -u "an3" \
-        -s "an3@${h}:/home/an3/backup/docs/eBooks" \
+        -s "${storage[eBooks]}" \
         -d "${PWD}"
 }
 
