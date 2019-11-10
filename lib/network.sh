ccb2f5def67d94e0a0d12d8f7a5077d09ab64122
Andrei Ehrlich <andrei.ehrlich@gmail.com>
2019-11-10 18:36:52 +0100

    retab files under lib folder
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- lib/network.sh
+++ lib/network.sh
@@ -9,65 +9,65 @@ source "${SCRIPTS_LIB_DIR}/runtime.sh"
 ###############################################################################
 
 conn2ssh() {
-    local p="${1}"
-    local u="${2:-${USER}}"
-    local h="${3:-${HOSTNAME}}"
+	local p="${1}"
+	local u="${2:-${USER}}"
+	local h="${3:-${HOSTNAME}}"
 
-    isInstalled ssh || return $(die);
+	isInstalled ssh || return $(die);
 
-    local cmd=""
-    if [ -z "$(grep ${h} ~/.ssh/known_hosts 2> /dev/null)" ]; then
-        cmd="$(which ssh) ${u}@${h}"
-    elif [ ${#p} -eq 0 ]; then
-        cmd="$(which ssh) ${u}@${h}"
-    else
-        isInstalled sshpass || \
-        cmd="$(which ssh) ${u}@${h}" && \
-        cmd="$(which sshpass) -p ${p} $(which ssh) ${u}@${h}"
-    fi
+	local cmd=""
+	if [ -z "$(grep ${h} ~/.ssh/known_hosts 2> /dev/null)" ]; then
+		cmd="$(which ssh) ${u}@${h}"
+	elif [ ${#p} -eq 0 ]; then
+		cmd="$(which ssh) ${u}@${h}"
+	else
+		isInstalled sshpass || \
+			cmd="$(which ssh) ${u}@${h}" && \
+			cmd="$(which sshpass) -p ${p} $(which ssh) ${u}@${h}"
+	fi
 
-    eval "${cmd}"
+	eval "${cmd}"
 }
 
 ###############################################################################
 
 doRsync() {
 
-    local usr=""
-    local src=""
-    local dst=""
-    local -i args=0
+	local usr=""
+	local src=""
+	local dst=""
+	local -i args=0
 
-    while [ $# -gt 0 ]; do
-        case "${1}" in
-            -u) [[ -n "${2}" ]] && usr="${2}" && ((args++)) && shift 1;;
-            -s) [[ -n "${2}" ]] && src="${2}" && ((args++)) && shift 1;;
-            -d) [[ -n "${2}" ]] && dst="${2}" && ((args++)) && shift 1;;
-        esac
-        shift 1
-    done
+	while [ $# -gt 0 ]; do
+		case "${1}" in
+			-u) [[ -n "${2}" ]] && usr="${2}" && ((args++)) && shift 1;;
+			-s) [[ -n "${2}" ]] && src="${2}" && ((args++)) && shift 1;;
+			-d) [[ -n "${2}" ]] && dst="${2}" && ((args++)) && shift 1;;
+		esac
+		shift 1
+	done
 
-    getLengthMin "${usr}" "${src}" "${dst}" && \
-        printf "%s(): one of arguments is empty! [%s] [%s] [%s]\n" \
-        ${FUNCNAME[0]} "${usr}" "${src}" "${dst}" >&2 && \
-        return $(die)
+	getLengthMin "${usr}" "${src}" "${dst}" && \
+		printf "%s(): one of arguments is empty! [%s] [%s] [%s]\n" \
+		${FUNCNAME[0]} "${usr}" "${src}" "${dst}" >&2 && \
+		return $(die)
 
-    isInstalled rsync ssh || return $(die)
+	isInstalled rsync ssh || return $(die)
 
-    local cmd=""
-    cmd+="$(which rsync)"
-    cmd+=" --rsh=\"$(which ssh) -l ${usr}\""
-    cmd+=" --verbose"           # be verbose during backup
-    cmd+=" --recursive"         # copy directories recursively
-    cmd+=" --delete-during"     # delete files in destination folder that are not available in source folder during backup
-    cmd+=" --force"             # force deletion of directories even if they are not empty
-    cmd+=" --times"             # preserve modification time stamps
-    cmd+=" --timeout=60"        # if no data is transferred during timeout [seconds] then the rsync will exit
-    cmd+=" --progress"          # show progress during backup is being performed
-    cmd+=" --human-readable"    # show numbers in a more human readable format"
+	local cmd=""
+	cmd+="$(which rsync)"
+	cmd+=" --rsh=\"$(which ssh) -l ${usr}\""
+	cmd+=" --verbose"           # be verbose during backup
+	cmd+=" --recursive"         # copy directories recursively
+	cmd+=" --delete-during"     # delete files in destination folder that are not available in source folder during backup
+	cmd+=" --force"             # force deletion of directories even if they are not empty
+	cmd+=" --times"             # preserve modification time stamps
+	cmd+=" --timeout=60"        # if no data is transferred during timeout [seconds] then the rsync will exit
+	cmd+=" --progress"          # show progress during backup is being performed
+	cmd+=" --human-readable"    # show numbers in a more human readable format"
 
-    echo "${cmd} ${src} ${dst}"
-    eval "${cmd} ${src} ${dst}"
+	echo "${cmd} ${src} ${dst}"
+	eval "${cmd} ${src} ${dst}"
 }
 
 ###############################################################################
--- lib/runtime.sh
+++ lib/runtime.sh
@@ -8,16 +8,16 @@ source "${SCRIPTS_LIB_DIR}/string.sh"
 
 # returns true when the script is sourced; false - when not
 isSourced() {
-    local len=${#BASH_SOURCE[@]}
-    test "${0}" != "${BASH_SOURCE[${len}-1]}"
-    return $?
+	local len=${#BASH_SOURCE[@]}
+	test "${0}" != "${BASH_SOURCE[${len}-1]}"
+	return $?
 }
 
 ###############################################################################
 
 # returns true when the script is executed; false - when not
 isExecuted() {
-    isSourced && return 1 || return 0
+	isSourced && return 1 || return 0
 }
 
 ###############################################################################
@@ -26,63 +26,63 @@ isExecuted() {
 # false - when not (number of missed utilities) & prints list of missing
 # executable files to stderr in this case.
 isInstalled() {
-    local missed="0"
-    while [ $# -gt 0 ]; do
-        local utility="${1}"
-        shift 1
-        which "${utility}" &> /dev/null && continue
-        [[ ${missed} -eq 0 ]] \
-            && printf "Following utilities are not available in \$PATH:\n" >&2
-        ((missed++))
-        printf "\t'%s'\n" "${utility}" >&2;
-    done
-    return ${missed};
+	local missed="0"
+	while [ $# -gt 0 ]; do
+		local utility="${1}"
+		shift 1
+		which "${utility}" &> /dev/null && continue
+		[[ ${missed} -eq 0 ]] \
+			&& printf "Following utilities are not available in \$PATH:\n" >&2
+					((missed++))
+					printf "\t'%s'\n" "${utility}" >&2;
+	done
+	return ${missed};
 }
 
 ###############################################################################
 
 die() {
-    [[ $# -gt 0 ]] && printf "$*\n"
-    printStackTraceFormatted >&2
-    exit 1
+	[[ $# -gt 0 ]] && printf "$*\n"
+	printStackTraceFormatted >&2
+	exit 1
 }
 
 ###############################################################################
 
 printStackTrace() {
-    # prints stack trace using 'caller' builtin directly
-    local frame=0
-    while caller ${frame}; do
-        ((frame++))
-    done
+	# prints stack trace using 'caller' builtin directly
+	local frame=0
+	while caller ${frame}; do
+		((frame++))
+	done
 }
 
 ###############################################################################
 
 printStackTraceFormatted() {
-    # prints stack trace in format FILE --> FUNCTION --> LINE
-    local separator="${1:- --> }"
+	# prints stack trace in format FILE --> FUNCTION --> LINE
+	local separator="${1:- --> }"
 
-    mapfile <<< "$(printStackTrace)"
-    for i in "${MAPFILE[@]}"; do
-        IFS=' ' set -- ${i}
-        local lines[${#lines[@]}]="${1}";
-        local funcs[${#funcs[@]}]="${2}";
-        local files[${#files[@]}]="${3}";
-    done
+	mapfile <<< "$(printStackTrace)"
+	for i in "${MAPFILE[@]}"; do
+		IFS=' ' set -- ${i}
+		local lines[${#lines[@]}]="${1}";
+		local funcs[${#funcs[@]}]="${2}";
+		local files[${#files[@]}]="${3}";
+	done
 
-    getLengthMax ${files[@]}
-    local len_files=$?
-    getLengthMax ${lines[@]}
-    local len_lines=$?
-    getLengthMax ${funcs[@]}
-    local len_funcs=$?
+	getLengthMax ${files[@]}
+	local len_files=$?
+	getLengthMax ${lines[@]}
+	local len_lines=$?
+	getLengthMax ${funcs[@]}
+	local len_funcs=$?
 
-    printf "%-${len_files}s${separator}%-${len_funcs}s${separator}%${len_lines}s\n" "FILE" "FUNCTION" "LINE"
-    for (( i=0; i<${#files[@]}; i++ )); do
-        printf "%-${len_files}s${separator}%-${len_funcs}s${separator}%${len_lines}d\n" \
-        "${files[$i]}" "${funcs[$i]}" "${lines[$i]}"
-    done
-}
+	printf "%-${len_files}s${separator}%-${len_funcs}s${separator}%${len_lines}s\n" "FILE" "FUNCTION" "LINE"
+	for (( i=0; i<${#files[@]}; i++ )); do
+		printf "%-${len_files}s${separator}%-${len_funcs}s${separator}%${len_lines}d\n" \
+			"${files[$i]}" "${funcs[$i]}" "${lines[$i]}"
+		done
+	}
 
 ###############################################################################
