80699d289025f12878c3d889220c6aac2428d36e
Andrei Ehrlich <andrei.ehrlich@gmail.com>
2021-06-08 12:09:06 +0200

    network.sh: be independent.
    - make the script independent on any other scripts.
    - enable a couple of important shell options.
    - use arrays in favour of a simple appending to a variable.
    
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- lib/network.sh
+++ lib/network.sh
@@ -1,43 +1,44 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This file contains functions that are often used to connect to other machines.
 #
 ###############################################################################
 
-source "${SCRIPTS_LIB}/runtime.sh"
+set -e
+set -u
+set -x
 
 ###############################################################################
 
 conn2ssh() {
-	local p="${1}"
-	local u="${2:-${USER}}"
-	local h="${3:-${HOSTNAME}}"
+	local -r pass_word="${1}";
+	local -r user_name="${2:-${USER}}";
+	local -r host_name="${3:-${HOSTNAME}}";
 
-	isInstalled ssh || return $(die);
+	which ssh &>/dev/null || return $?;
 
-	local cmd=""
-	if [ -z "$(grep ${h} ~/.ssh/known_hosts 2> /dev/null)" ]; then
-		cmd="$(which ssh) ${u}@${h}"
-	elif [ ${#p} -eq 0 ]; then
-		cmd="$(which ssh) ${u}@${h}"
+	if [[ -n "${pass_word}" ]]; then
+		which sshpass &>/dev/null || \
+			eval "ssh ${user_name}@${host_name}" && \
+			eval "sshpass -p ${pass_word} ssh ${user_name}@${host_name}";
 	else
-		isInstalled sshpass || \
-			cmd="$(which ssh) ${u}@${h}" && \
-			cmd="$(which sshpass) -p ${p} $(which ssh) ${u}@${h}"
+		eval "ssh ${user_name}@${host_name}";
 	fi
-
-	eval "${cmd}"
 }
 
 ###############################################################################
 
 doRsync() {
 
+	for exe in rsync ssh; do
+		which "${exe}" &>/dev/null || return $?;
+	done
+
 	local usr="";
 	local src="";
 	local dst="";
 
-	while [ $# -gt 0 ]; do
+	while [[ $# -gt 0 ]]; do
 		case "${1}" in
 			-u) [[ -n "${2}" ]] && usr="${2}" && shift 1;;
 			-s) [[ -n "${2}" ]] && src="${2}" && shift 1;;
@@ -46,27 +47,20 @@ doRsync() {
 		shift 1
 	done
 
-	getLengthMin "${usr}" "${src}" "${dst}" && \
-		printf "%s(): one of arguments is empty! [%s] [%s] [%s]\n" \
-		${FUNCNAME[0]} "${usr}" "${src}" "${dst}" >&2 && \
-		return $(die)
+	local cmd=("rsync");
+	cmd+=("--rsh=\"$(which ssh) -l ${usr:?Please specify user name}\"");
+	cmd+=("--verbose");			# be verbose during backup
+	cmd+=("--recursive");		# copy directories recursively
+	cmd+=("--delete-during");	# delete files in destination folder that are not available in source folder during backup
+	cmd+=("--force");			# force deletion of directories even if they are not empty
+	cmd+=("--times");			# preserve modification time stamps
+	cmd+=("--timeout=60");		# if no data is transferred during timeout [seconds] then the rsync will exit
+	cmd+=("--progress");		# show progress during backup is being performed
+	cmd+=("--human-readable");	# show numbers in a more human readable format"
+	cmd+=("${src:?Please specify source directory for backup}");
+	cmd+=("${dst:?Please specify destination directory for backup}");
 
-	isInstalled rsync ssh || return $(die)
-
-	local cmd=""
-	cmd+="$(which rsync)"
-	cmd+=" --rsh=\"$(which ssh) -l ${usr}\""
-	cmd+=" --verbose"           # be verbose during backup
-	cmd+=" --recursive"         # copy directories recursively
-	cmd+=" --delete-during"     # delete files in destination folder that are not available in source folder during backup
-	cmd+=" --force"             # force deletion of directories even if they are not empty
-	cmd+=" --times"             # preserve modification time stamps
-	cmd+=" --timeout=60"        # if no data is transferred during timeout [seconds] then the rsync will exit
-	cmd+=" --progress"          # show progress during backup is being performed
-	cmd+=" --human-readable"    # show numbers in a more human readable format"
-
-	echo "${cmd} ${src} ${dst}"
-	eval "${cmd} ${src} ${dst}"
+	eval "${cmd[@]}";
 }
 
 ###############################################################################
